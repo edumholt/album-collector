eea5aead6df03977e714baadb646318f49a7c827
Edward Dumholt <edumholt@gmail.com>
2020-03-01 20:18:24 -0600

    form test

--- src/components/Album/Album.tsx
+++ src/components/Album/Album.tsx
@@ -3,5 +3,16 @@ import React from 'react';
 interface AlbumProps {}
 
 export const Album: React.FC<AlbumProps> = props => {
-  return <h1>Album</h1>;
+  return (
+    <>
+      <h1>Album</h1>
+      <form name="contact" data-netlify="true">
+        <label htmlFor="firstName">First Name</label>
+        <input type="text" id="firstName" name="firstName" placeholder="First Name" />
+        <label htmlFor="lastName">Last Name</label>
+        <input type="text" id="lastName" name="lastName" placeholder="Last Name" />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 };
