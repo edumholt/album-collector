80783faa49f67c1a1375a8561e6583b0f941b58e
Edward Dumholt <edumholt@pesi.com>
2020-03-02 06:25:15 -0600

    put form in index.html

--- src/components/Album/Album.tsx
+++ src/components/Album/Album.tsx
@@ -3,16 +3,5 @@ import React from 'react';
 interface AlbumProps {}
 
 export const Album: React.FC<AlbumProps> = props => {
-  return (
-    <>
-      <h1>Album</h1>
-      <form name="contact" data-netlify="true">
-        <label htmlFor="firstName">First Name</label>
-        <input type="text" id="firstName" name="firstName" placeholder="First Name" />
-        <label htmlFor="lastName">Last Name</label>
-        <input type="text" id="lastName" name="lastName" placeholder="Last Name" />
-        <button type="submit">Submit</button>
-      </form>
-    </>
-  );
+  return <h1>Album</h1>;
 };
